ceeadc31d21701549f6670006ad318503b1f967d
JP <josiah.peterson@wsu.edu>
2019-02-05 13:23:14 -0800

    Uploading project

---
+++ DictionaryApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DictionaryApp.Models;
+
+namespace DictionaryApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string word)
+        {
+            // Retrieve data source content 'Datasource/words_alpha.txt' and define pattern 
+            // for parsing starting and containing strings. Insert 'word' url param to dictate 
+            // what to search for.
+            string word_content = System.IO.File.ReadAllText("Datasource/words_alpha.txt");
+            Search search = new Search(word, word_content);
+            search.search();
+            // Check if the search term is a valid search
+            if (search.isValid())
+            {
+                // Set variables for HTML search page to use.
+                ViewData["mainResults"] = search.getMainMatches();
+                ViewData["relatedResults"] = search.getRelatedMatches();
+                ViewData["search"] = word;
+                ViewData["isSearching"] = true;
+            }
+            // Otherwise, don't view the search items
+            else
+            {
+                ViewData["isSearching"] = false;
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DictionaryApp/Search.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DictionaryApp
+{
+    public class Search
+    {
+        // Search term and the content to be searched,
+        // as well as search properties for regex
+        private string searchTerm;
+        private string content;
+        private string searchProperties = @"(?im)^";
+        // The pattern strings to be used for the search
+        private string mainPattern;
+        private string relatedPattern;
+        // Checks whether search term was valid or not
+        private bool isvalid;
+        // Match collections of the main and related matches
+        // obtained from the content
+        private MatchCollection mainMatches;
+        private MatchCollection relatedMatches;
+        
+        // Search
+        // Parameters:
+        // searchTerm (string) - The search term to look for in content
+        // content (string) - The content to be searched using searchTerm
+        public Search(string searchTerm, string content)
+        {
+            this.searchTerm = searchTerm;
+            this.content = content;
+            this.mainPattern = this.searchProperties + this.searchTerm + ".*";
+            this.relatedPattern = this.searchProperties + ".+" + this.searchTerm + ".*";
+        }
+        // method: search
+        // Obtains both the main and related matches of type MatchCollection if the
+        // search function has been called successfully with a valid search term.
+        // The items of mainMatches and relatedMatches will be in lexicographical order.
+        // If there is an issue with the search term, the isvalid value will 
+        // be set to false. Otherwise, it will be set to true. An Exception
+        // object will be returned if there is an issue. Otherwise, null will
+        // be returned.
+        public Exception search()
+        {
+            // Check if search term is not null and length is longer than 0
+            if (this.searchTerm != null && this.searchTerm.Length > 0)
+            {
+                // Check for unexpected errors
+                try
+                {
+                    this.mainMatches = Regex.Matches(this.content, this.mainPattern);
+                    this.relatedMatches = Regex.Matches(this.content, this.relatedPattern);
+                    if (this.mainMatches != null && this.relatedMatches != null)
+                    {
+                        this.isvalid = true;
+                        return null;
+                    }
+                    this.isvalid = false;
+                    return new Exception("Unexpected error occurred");
+                }
+                // If there are unexpected errors, return exception and set isvalid to false
+                catch (Exception e)
+                {
+                    this.mainMatches = null;
+                    this.relatedMatches = null;
+                    this.isvalid = false;
+                    return e;
+                }
+            }
+            // If the search term is null and/or has a length of 0, return false
+            // and exception
+            else
+            {
+                this.isvalid = false;
+                return new Exception("There were no search terms entered.");
+            }
+        }
+        // method: getMainMatches
+        // Returns the main matches as MatchCollection if successful.
+        // Otherwise, it returns null if unsuccessful or no search conducted.
+        public MatchCollection getMainMatches()
+        {
+            return this.mainMatches;
+        }
+        // method: getRelatedMatches
+        // Returns the related matches as MatchCollection if successful.
+        // Otherwise, it returns null if unsuccessful or no search conducted.
+        public MatchCollection getRelatedMatches()
+        {
+            return this.relatedMatches;
+        }
+        // method: isValid
+        // Returns if the search term used was valid
+        public bool isValid()
+        {
+            return this.isvalid;
+        }
+    }
+}
